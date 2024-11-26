89501a53d77d7f59a7a67d2e648a7ba44715efcb
nikolaimurtazin <murtazinnikol@yandex.ru>
2024-11-26 11:10:29 +0100

    "feat: добавил Gateway"

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/warehouse/feign/WarehouseClient.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/warehouse/feign/WarehouseClient.java
@@ -4,11 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import ru.yandex.practicum.shoppingCart.dto.ShoppingCartDto;
+import ru.yandex.practicum.warehouse.congif.WarehouseClientFallback;
 import ru.yandex.practicum.warehouse.dto.AssemblyProductForOrderFromShoppingCartRequest;
 import ru.yandex.practicum.warehouse.dto.BookedProductDto;
 
 
-@FeignClient(name = "warehouse-service", path = "/api/v1/warehouse")
+@FeignClient(name = "warehouse-service", path = "/api/v1/warehouse", fallback = WarehouseClientFallback.class)
 public interface WarehouseClient {
 
     @PostMapping("/booking")
